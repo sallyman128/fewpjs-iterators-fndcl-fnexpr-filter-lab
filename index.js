af622caceae80cd849646f7fdd4d12d26c0666c2
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-02 09:55:53 -0400

    commit tree 7ec527ba1248087ae9d6b50c9f409478c0b18854

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(driversArray, condition) {
+  return driversArray.filter( (driver) => {
+    return driver.toLowerCase() === condition.toLowerCase();
+  })
+}
+
+function fuzzyMatch(driversArray, condition) {
+  return driversArray.filter( (driver) => {
+    let num = condition.length;
+    return driver.slice(0, num) === condition;
+  })
+}
+
+function matchName(driversArray, condition) {
+  return driversArray.filter( (driverObject) => {
+    return driverObject.name === condition;
+  })
+}
